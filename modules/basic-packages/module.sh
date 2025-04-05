a37360f5f069e7fb2754c99a637bc39c3329c14b
JongHan Leem <jleem42@gmail.com>
2025-04-05 22:00:11 +0900

    wip

--- modules/basic-packages/module.sh
+++ modules/basic-packages/module.sh
@@ -13,16 +13,16 @@ if [ ! -d ~/.config/nvim ]; then
   git clone https://github.com/LazyVim/starter ~/.config/nvim
   rm -rf ~/.config/nvim/.git
 
-  cat <<EOF >> ~/.config/nvim/lua/config/keymaps.lua
-  -- Map <CR> to ciw in normal mode
-  vim.keymap.set('n', '<CR>', 'ciw', { noremap = true, silent = true, desc = "Change inner word" })
+  cat <<EOF > ~/.config/nvim/lua/config/keymaps.lua
+-- Map <CR> to ciw in normal mode
+vim.keymap.set('n', '<CR>', 'ciw', { noremap = true, silent = true, desc = "Change inner word" })
 
-  -- Map ; to : in normal and visual modes
-  vim.keymap.set({ 'n', 'v' }, ';', ':', { noremap = true, silent = false, desc = "Map ; to :" })
+-- Map ; to : in normal and visual modes
+vim.keymap.set({ 'n', 'v' }, ';', ':', { noremap = true, silent = false, desc = "Map ; to :" })
 
-  -- Map %% in command-line mode to expand('%:p:h').'/'
-  vim.keymap.set('c', '%%', "<C-R>=expand('%:p:h').'/'<CR>", { noremap = true, silent = true, desc = "Expand current file path in command-line mode" })
-  EOF
+-- Map %% in command-line mode to expand('%:p:h').'/'
+vim.keymap.set('c', '%%', "<C-R>=expand('%:p:h').'/'<CR>", { noremap = true, silent = true, desc = "Expand current file path in command-line mode" })
+EOF
 fi
 
 log_success "Basic packages installed successfully!" 
