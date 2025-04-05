8edbbc80cffc8f48c2caed8d9378f9deb7af8bf2
JongHan Leem <jleem42@gmail.com>
2025-04-05 22:13:41 +0900

    wip

--- modules/basic-packages/debian.sh
+++ modules/basic-packages/debian.sh
@@ -21,8 +21,5 @@ curl -LSfs https://raw.githubusercontent.com/Byron/dua-cli/master/ci/install.sh 
     sh -s -- --git Byron/dua-cli --target x86_64-unknown-linux-musl --crate dua --tag v2.29.0
 
 # Add dua-cli to PATH
-
-
-set +u
-source $(get_shell_profile)
-set -u
+add_to_path "$HOME/.cargo/bin"
+log_info "You may need to restart your terminal to use installed packages"
