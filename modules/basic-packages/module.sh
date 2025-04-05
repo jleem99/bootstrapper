3899b505bb1074c2537b83a874a6b97d5acd05ee
JongHan Leem <jleem42@gmail.com>
2025-04-05 21:52:16 +0900

    wip

--- modules/basic-packages/module.sh
+++ modules/basic-packages/module.sh
@@ -6,4 +6,21 @@ log_info "Installing basic packages..."
 # Run platform-specific implementation
 module_run_platform "basic-packages"
 
+# Setup neovim
+log_info "Setting up neovim..."
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+cat <<EOF >> ~/.config/nvim/lua/config/keymaps.lua
+-- Map <CR> to ciw in normal mode
+vim.keymap.set('n', '<CR>', 'ciw', { noremap = true, silent = true, desc = "Change inner word" })
+
+-- Map ; to : in normal and visual modes
+vim.keymap.set({ 'n', 'v' }, ';', ':', { noremap = true, silent = false, desc = "Map ; to :" })
+
+-- Map %% in command-line mode to expand('%:p:h').'/'
+vim.keymap.set('c', '%%', "<C-R>=expand('%:p:h').'/'<CR>", { noremap = true, silent = true, desc = "Expand current file path in command-line mode" })
+EOF
+
 log_success "Basic packages installed successfully!" 
