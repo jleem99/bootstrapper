d665e27491f518f467f8aa29848d8b6057327585
JongHan Leem <jleem42@gmail.com>
2026-06-24 16:18:13 +0900

    feat: update zsh module for non-interactive oh-my-zsh installation and improved starship setup

--- modules/zsh/module.sh
+++ modules/zsh/module.sh
@@ -7,20 +7,22 @@ ensure_packages_installed "zsh" "curl" "git"
 
 try_run "Set default shell to zsh" chsh -s "$(which zsh)"
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install oh-my-zsh (non-interactively)
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/^plugins=(/plugins=(zsh-syntax-highlighting /' ~/.zshrc
+perl -pi -e 's/^plugins=\(/plugins=\(zsh-syntax-highlighting /' ~/.zshrc
 
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i 's/^plugins=(/plugins=(zsh-autosuggestions /' ~/.zshrc
+perl -pi -e 's/^plugins=\(/plugins=\(zsh-autosuggestions /' ~/.zshrc
 
-# Install starship
-curl -sS https://starship.rs/install.sh | sh
-echo 'eval "$(starship init '$(get_current_shell)')"' >> "$(get_shell_profile)"
+# Install starship (clearing shell env variables to avoid POSIX warnings, and auto-confirming)
+curl -sS https://starship.rs/install.sh | env -u BASH_VERSION -u ZSH_VERSION sh -s -- --yes
+if ! grep -q "starship init zsh" ~/.zshrc; then
+  echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+fi
 
 # Add bootstrapper to PATH
 BIN_DIR="$HOME/.local/bin"
